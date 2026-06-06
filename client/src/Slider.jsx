c0d350523797b6f0f3878dea0eacba0d2bbfeff0
Puneet Kaur <puneetjas35@gmail.com>
2026-06-06 11:56:42 +0530

    Update Slider.jsx

--- client/src/Slider.jsx
+++ client/src/Slider.jsx
@@ -20,7 +20,7 @@ const ImageCarousel = () => {
         <img
           src="https://nutriorg.com/cdn/shop/files/Juice_Web.jpg?v=1754546468"
           alt="Image 1"
-         className='w-full h-[220px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover cursor-pointer'
+         className='w-full h-auto object-contain cursor-pointer'
         />
         </Link>
       </SwiperSlide>
@@ -31,7 +31,7 @@ const ImageCarousel = () => {
         <img
           src="https://nutriorg.com/cdn/shop/files/Oil_web.jpg?v=1754546468"
           alt="Image 2"
-         className='w-full h-[220px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover cursor-pointer'
+         className='w-full h-auto object-contain cursor-pointer'
         />
         </Link>
       </SwiperSlide>
@@ -43,7 +43,7 @@ const ImageCarousel = () => {
         <img
           src="https://nutriorg.com/cdn/shop/files/Sea_Buckthorn_Web_828b8ef8-07a7-467f-94b1-23623e3481de.jpg?v=1754546592"
           alt="Image 3"
-          className='w-full h-[220px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover cursor-pointer'
+          className='w-full h-auto object-contain cursor-pointer'
         />
         </Link>
       </SwiperSlide>
