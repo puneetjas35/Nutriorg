2d51453f1b8441e85bdc2b384772538e8242caad
Puneet Kaur <puneetjas35@gmail.com>
2026-06-10 16:45:50 +0530

    Update ShopCategory.jsx

--- client/src/ShopCategory.jsx
+++ client/src/ShopCategory.jsx
@@ -39,29 +39,32 @@ const ShopCategory = () => {
       breakpoint: 1536,
       settings: {
         slidesToShow: 4,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 1280,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 1024,
+      breakpoint: 992,
       settings: {
         slidesToShow: 2,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 640,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
+        slidesToScroll: 1,
       },
     },
   ],
 };
-
 
   return (
     <section className='border-top collection-section py-4 overflow-visible'>
