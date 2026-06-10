8e2f8b08d605fe40d3cee7b53038f9628f5bdc9b
Puneet Kaur <puneetjas35@gmail.com>
2026-06-10 16:03:17 +0530

    Update Slider.jsx

--- client/src/Slider.jsx
+++ client/src/Slider.jsx
@@ -2,7 +2,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { Link } from "react-router-dom"
 import 'swiper/css';
-// import 'swiper/css/navigation';
 
 const ImageCarousel = () => {
   return (
@@ -10,7 +9,6 @@ const ImageCarousel = () => {
       modules={[Pagination, Autoplay]}
       loop={true}
       spaceBetween={20}
-      {/* navigation */}
       autoplay={{ delay: 3500 }}
       slidesPerView={1}
       pagination={{ clickable: true }}
