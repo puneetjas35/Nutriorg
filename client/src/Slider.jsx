be56d694f44208ef9bddd814df2db85863d18742
Puneet Kaur <puneetjas35@gmail.com>
2026-06-10 16:00:05 +0530

    Update Slider.jsx

--- client/src/Slider.jsx
+++ client/src/Slider.jsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { Link } from "react-router-dom"
 import 'swiper/css';
-import 'swiper/css/navigation';
+// import 'swiper/css/navigation';
 
 const ImageCarousel = () => {
   return (
@@ -10,7 +10,7 @@ const ImageCarousel = () => {
       modules={[Pagination, Autoplay]}
       loop={true}
       spaceBetween={20}
-      navigation
+      {/* navigation */}
       autoplay={{ delay: 3500 }}
       slidesPerView={1}
       pagination={{ clickable: true }}
@@ -18,11 +18,18 @@ const ImageCarousel = () => {
       <SwiperSlide>
         <Link to="/product/aloeamla">
         <div className="w-full h-[220px] sm:h-[280px] md:h-[350px] lg:h-[450px] overflow-hidden">
-        <img
-          src="https://nutriorg.com/cdn/shop/files/Juice_Web.jpg?v=1754546468"
-          alt="Image 1"
-        className="w-full h-full object-cover cursor-pointer"
-        />
+          <picture>
+              <source
+                media="(max-width: 767px)"
+                srcSet="https://nutriorg.com/cdn/shop/files/Web_Ver_copy_1.jpg?v=1779805449"
+              />
+            <img
+              src="https://nutriorg.com/cdn/shop/files/Juice_Web.jpg?v=1754546468"
+              alt="Image 1"
+              className="w-full h-full object-cover cursor-pointer"
+              draggable="false"
+            />
+         </picture>
         </div>
         </Link>
       </SwiperSlide>
@@ -31,11 +38,19 @@ const ImageCarousel = () => {
       <SwiperSlide>
         <Link to="/category/cold-pressed-oils">
         <div className="w-full h-[220px] sm:h-[280px] md:h-[350px] lg:h-[450px] overflow-hidden">
-        <img
-          src="https://nutriorg.com/cdn/shop/files/Oil_web.jpg?v=1754546468"
-          alt="Image 2"
-        className="w-full h-full object-cover cursor-pointer"
-        />
+        <picture>
+            <source
+              media="(max-width: 767px)"
+              srcSet="https://nutriorg.com/cdn/shop/files/1-_SBT_Tri.jpg?v=1779358987"
+            />
+            <img
+              src="https://nutriorg.com/cdn/shop/files/Oil_web.jpg?v=1754546468"
+              alt="Image 2"
+              className="w-full h-full object-cover cursor-pointer"
+              loading="lazy"
+              draggable="false"
+            />
+        </picture>
         </div>
         </Link>
       </SwiperSlide>
@@ -45,11 +60,19 @@ const ImageCarousel = () => {
       <SwiperSlide>
         <Link to="/product/seabuckthorn">
         <div className="w-full h-[220px] sm:h-[280px] md:h-[350px] lg:h-[450px] overflow-hidden">
-        <img
-          src="https://nutriorg.com/cdn/shop/files/Sea_Buckthorn_Web_828b8ef8-07a7-467f-94b1-23623e3481de.jpg?v=1754546592"
-          alt="Image 3"
-          className="w-full h-full object-cover cursor-pointer"
-        />
+        <picture>
+          <source
+            media="(max-width: 767px)"
+            srcSet="https://nutriorg.com/cdn/shop/files/Nutriorg_BAU_banner_web_1.jpg?v=1779691387"
+          />
+          <img
+            src="https://nutriorg.com/cdn/shop/files/Sea_Buckthorn_Web_828b8ef8-07a7-467f-94b1-23623e3481de.jpg?v=1754546592"
+            alt="Image 3"
+            className="w-full h-full object-cover cursor-pointer"
+            loading="lazy"
+            draggable="false"
+          />
+      </picture>
         </div>
         </Link>
       </SwiperSlide>
