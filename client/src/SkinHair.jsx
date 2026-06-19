853ffaf20d86a8d13499f44575313dc571f05f8e
Puneet Kaur <puneetjas35@gmail.com>
2026-06-19 17:39:25 +0530

    Update SkinHair.jsx

--- client/src/SkinHair.jsx
+++ client/src/SkinHair.jsx
@@ -112,7 +112,7 @@ const SkinHair = () => {
         });
 
     return (
-        <section className="skincare bg-[rgb(244,242,233)] mt-12 md:mt-12 pb-8">
+        <section className="skincare bg-[rgb(244,242,233)] pt-8 md:pt-10 pb-8">
             {/* Heading */}
            <div className="heading px-4 md:px-6 py-2 md:py-4 flex items-center justify-between">
                 <h2 className="text-[18px] md:text-[24px] font-semibold uppercase tracking-normal text-[#9A8F96] hover:text-[#6a5b5c] transition-colors duration-300">
